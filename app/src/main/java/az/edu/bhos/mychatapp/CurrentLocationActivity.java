50d7f52e985c2658b692b0972fbbd79b6d15927b
Roman Tolstosheyev <rtolstosheyev@intershop.com>
2023-09-10 23:04:30 +0400

    create an empty activity

---
+++ app/src/main/java/az/edu/bhos/mychatapp/CurrentLocationActivity.java
@@ -0,0 +1,14 @@
+package az.edu.bhos.mychatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_location_acitvity);
+    }
+}
