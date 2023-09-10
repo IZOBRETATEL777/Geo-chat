390d7d826c24b0b6f50a763dd7fa4d13d8e9c4e3
Roman Tolstosheyev <rtolstosheyev@intershop.com>
2023-09-10 23:14:18 +0400

    Add layout

--- app/src/main/java/az/edu/bhos/mychatapp/CurrentLocationActivity.java
+++ app/src/main/java/az/edu/bhos/mychatapp/CurrentLocationActivity.java
@@ -9,6 +9,6 @@ public class CurrentLocationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_current_location_acitvity);
+        setContentView(R.layout.activity_current_location);
     }
 }
